45cb790d7e45a687baac38e3c778739278ab4281
Benjamin <bmaycock854@gmail.com>
2023-12-16 19:06:31 -0600

    assignment

---
+++ week005.java
@@ -0,0 +1,60 @@
+package Week5;
+
+public class week005 {
+
+	    public interface Logger {
+	        void log(String message);
+	        void error(String message);
+	    }
+
+	    public static class ConcreteAsteriskLogger extends AsteriskLogger {
+	        // Concrete implementation if needed
+	    }
+
+	    public static class ConcreteSpacedLogger extends SpacedLogger {
+	        // Concrete implementation if needed
+	    }
+
+	    public abstract static class AsteriskLogger implements Logger {
+	        @Override
+	        public void log(String message) {
+	            System.out.println("***" + message + "***");
+	        }
+
+	        @Override
+	        public void error(String message) {
+	            System.out.println("*****************");
+	            System.out.println("***Error: " + message + "***");
+	            System.out.println("*****************");
+	        }
+	    }
+
+	    public abstract static class SpacedLogger implements Logger {
+	        @Override
+	        public void log(String message) {
+	            for (char c : message.toCharArray()) {
+	                System.out.print(c + " ");
+	            }
+	            System.out.println();
+	        }
+
+	        @Override
+	        public void error(String message) {
+	            System.out.println("Error: ");
+	            for (char c : message.toCharArray()) {
+	                System.out.print(c + " ");
+	            }
+	            System.out.println();
+	        }
+	    }
+
+	    public static class App {
+	        public static void main(String[] args) {
+	            ConcreteAsteriskLogger concreteAsteriskLogger = new ConcreteAsteriskLogger();
+	            ConcreteSpacedLogger concreteSpacedLogger = new ConcreteSpacedLogger();
+
+	            concreteAsteriskLogger.log("Hello");
+	            concreteSpacedLogger.log("Hello");
+	        }
+	    }
+	}
